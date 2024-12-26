10b9373cfe4fa723680bc9d8f7422041e2df95cd
Deo <diothewind@gmail.com>
2024-12-26 10:37:04 +0800

    Finish 5th challenge in exercise 6.0

--- 6.0 DIY API/index.js
+++ 6.0 DIY API/index.js
@@ -48,6 +48,20 @@ app.post("/jokes", (req, res) => {
 });
 
 //5. PUT a joke
+app.put("/jokes/:id", (req, res) => {
+  const id = Number(req.params.id);
+
+  const editedJoke = {
+    id: id,
+    jokeText: req.body.text,
+    jokeType: req.body.type
+  };
+
+  const index = jokes.findIndex(joke => joke.id === id);
+  jokes[index] = editedJoke;
+
+  res.json(editedJoke);
+})
 
 //6. PATCH a joke
 
