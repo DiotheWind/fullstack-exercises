c26a0d8f1bd2fe565a5b8fc1f9f21e3e8b9410de
Deo <diothewind@gmail.com>
2024-12-26 11:04:10 +0800

    Finish 7th challenge in exercise 6.0

--- 6.0 DIY API/index.js
+++ 6.0 DIY API/index.js
@@ -84,6 +84,19 @@ app.patch("/jokes/:id", (req, res) => {
 })
 
 //7. DELETE Specific joke
+app.delete("/jokes/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const index = jokes.findIndex(joke => joke.id === id);
+
+  if (index > -1) {
+    jokes.splice(index, 1);
+    res.sendStatus(200);
+  } else {
+    res.status(404).json({
+      error: `The joke with the id of ${id} cannot be found...`
+    });
+  }
+})
 
 //8. DELETE All jokes
 
